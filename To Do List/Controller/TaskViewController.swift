2dd215c9f16b46212e140e4326b5ce5bfa5dcc0b
Natalia Sakai <nataliasakai31@gmail.com>
2022-03-01 09:43:56 -0300

    task controller

---
+++ To Do List/Controller/TaskViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetalheViewController.swift
+//  To Do List
+//
+//  Created by Natalia Sakai on 01/03/22.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseFirestore
+
+class TaskViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
